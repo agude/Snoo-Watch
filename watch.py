46dda2829b7d317623b73ad703fc8b2221dd20a4
Alexander Gude <alex.public.account@gmail.com>
2018-01-01 17:12:48 -0800

    Add logging

--- watch.py
+++ watch.py
@@ -2,6 +2,7 @@
 
 
 import argparse
+import logging
 import praw
 import sqlite3
 
@@ -22,6 +23,7 @@ seen integer NOT NULL
 );
     """
 
+    logging.info("Creating database: %s", database_file)
     with sqlite3.connect(database_file) as con:
         con.execute(POSTS_TABLE)
         con.execute(USER_TABLE)
@@ -32,28 +34,33 @@ def check_results(cursor):
 
 
 def post_already_processed(post_id, cursor):
+    logging.debug("Checking post with ID '%s'", post_id)
     CHECK_SUBMISSION_TEMPLATE = 'SELECT EXISTS(SELECT 1 FROM posts WHERE id="{id}" LIMIT 1);'
     cursor.execute(CHECK_SUBMISSION_TEMPLATE.format(id=post_id))
     return check_results(cursor)
 
 
 def user_exists(name, cursor):
+    logging.debug("Checking user with name '%s'", name)
     CHECK_USER_TEMPLATE = 'SELECT EXISTS(SELECT 1 FROM users WHERE name="{name}" LIMIT 1);'
     cursor.execute(CHECK_USER_TEMPLATE.format(name=name))
     return check_results(cursor)
 
 
 def insert_post(post_id, cursor):
+    logging.debug("Inserting post with ID '%s'", post_id)
     INSERT_SUBMISSION_TEMPLATE = 'INSERT INTO posts (id) VALUES ("{id}")'
     cursor.execute(INSERT_SUBMISSION_TEMPLATE.format(id=post_id))
 
 
 def insert_new_user(name, cursor):
+    logging.debug("Inserting new user with name '%s'", name)
     INSERT_NEW_USER_TEMPLATE = 'INSERT INTO users (name, seen) VALUES ("{name}", 1);'
     cursor.execute(INSERT_NEW_USER_TEMPLATE.format(name=name))
 
 
 def update_user(name, cursor):
+    logging.debug("Updating existing user with name '%s'", name)
     UPDATE_USER_TEMPLATE = 'UPDATE users SET seen = seen + 1 WHERE name="{name}";'
     cursor.execute(UPDATE_USER_TEMPLATE.format(name=name))
 
@@ -61,21 +68,31 @@ def update_user(name, cursor):
 def extract_commenters(subreddit, time_aggregate, cursor):
     # Look at submissions
     for submission in subreddit.top(time_aggregate, limit=None):
+        logging.info("Checking submission: '%s' %s", submission.title, submission.id)
 
         # Skip if we have already processed the post
         if post_already_processed(submission.id, cursor):
+            logging.debug("Submission already processed, skipping!")
             continue
 
         # Insert the post
         insert_post(submission.id, cursor)
 
         # Get all commenting users
-        submission.comments.replace_more(limit=None)
-        for comment in submission.comments.list():
+        submission.comments.replace_more(limit=250)
+        comments = submission.comments.list()
+        total = len(comments)
+        for i, comment in enumerate(comments):
+            logging.debug("Comment %i: %s", i, comment.permalink)
+            # Log progress five times during the loop
+            if not i % (total // 5):
+                logging.info("Processing comment %i / %i", i+1, total)
+
             redditor = comment.author
             # Sometimes blank users come through due to deleted posts or other
             # circumstances, we just ignore them
             if redditor is None:
+                logging.debug("Commentor is empty, skipping!")
                 continue
 
             name = redditor.name
@@ -116,10 +133,30 @@ def main():
         help="scrape the comments of the top posts aggregated by this time",
         default='day',
     )
+    argparser.add_argument(
+        "-l",
+        "--log",
+        help="set the logging level, defaults to WARNING",
+        dest="log_level",
+        default=logging.WARNING,
+        choices=[
+            'DEBUG',
+            'INFO',
+            'WARNING',
+            'ERROR',
+            'CRITICAL',
+        ],
+    )
 
     args = argparser.parse_args()
 
+    # Set the logging level
+    logging.basicConfig(level=args.log_level)
+
+    logging.debug("Arguments: %s", args)
+
     # Set up the Reddit API
+    logging.info("Setting up Reddit API")
     reddit = praw.Reddit(
         client_id=APP_ID,
         client_secret=SECRET,
@@ -127,12 +164,15 @@ def main():
     )
 
     # Get the subreddit
+    logging.info("Getting subreddit: %s", args.subreddit)
     subreddit = reddit.subreddit(args.subreddit)
 
     # Set up the database
     setup_database(args.database)
+    logging.info("Connecting to database: %s", args.database)
     with sqlite3.connect(args.database) as con:
         cursor = con.cursor()
+
         extract_commenters(subreddit, args.top, cursor)
 
 
